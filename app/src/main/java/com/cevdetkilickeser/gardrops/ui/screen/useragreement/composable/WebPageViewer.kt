cac800f3aab77b0beb44e92696424915b1119856
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-12-25 09:18:12 +0300

    UserAgreement,  PrivacyPolicy -> Screens added

---
+++ app/src/main/java/com/cevdetkilickeser/gardrops/ui/screen/useragreement/composable/WebPageViewer.kt
@@ -0,0 +1,23 @@
+package com.cevdetkilickeser.gardrops.ui.screen.useragreement.composable
+
+import android.annotation.SuppressLint
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun WebPageViewer(url: String) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    })
+}
