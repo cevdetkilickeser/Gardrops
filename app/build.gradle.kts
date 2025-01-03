fe5a4a43574b865a31b529bec6684a6880d8cfc2
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2025-01-03 12:29:58 +0300

    Coil and Material Icons -> dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,13 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
+
+    //Coil
+    implementation(libs.coil.compose)
+
+    implementation (libs.androidx.material.icons.extended)
+
+
 }
 
 kapt {
