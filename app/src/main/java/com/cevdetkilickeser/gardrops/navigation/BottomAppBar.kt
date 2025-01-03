974e5ff2b19ed7090600bb328bf5344b511f53e2
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2025-01-03 12:30:21 +0300

    BottomAppBar -> added

---
+++ app/src/main/java/com/cevdetkilickeser/gardrops/navigation/BottomAppBar.kt
@@ -0,0 +1,89 @@
+package com.cevdetkilickeser.gardrops.navigation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.AddBox
+import androidx.compose.material.icons.outlined.ChatBubbleOutline
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.cevdetkilickeser.gardrops.R
+
+@Composable
+fun BottomAppBar(
+    currentDestination: Screen,
+    onHomeClick: () -> Unit
+) {
+    val selected = remember { mutableStateOf("Home") }
+    Column {
+        HorizontalDivider(color = Color.LightGray, thickness = 0.5.dp)
+        NavigationBar(
+            containerColor = MaterialTheme.colorScheme.background,
+            modifier = Modifier.height(70.dp)
+        ) {
+            NavigationBarItem(
+                icon =  { Icon(painter = painterResource(R.drawable.png_menu_icon), contentDescription = null) },
+                selected = selected.value == "Home",
+                onClick = { selected.value = "Home" },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onBackground,
+                    indicatorColor = Color.Transparent
+                )
+            )
+            NavigationBarItem(
+                icon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
+                selected = selected.value == "Search",
+                onClick = { selected.value = "Search" },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onBackground,
+                    indicatorColor = Color.Transparent
+                )
+            )
+            NavigationBarItem(
+                icon = { Icon(imageVector = Icons.Outlined.AddBox, contentDescription = null) },
+                selected = selected.value == "Add",
+                onClick = { selected.value = "Add" },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onBackground,
+                    indicatorColor = Color.Transparent
+                )
+            )
+            NavigationBarItem(
+                icon = { Icon(imageVector = Icons.Outlined.ChatBubbleOutline, contentDescription = null) },
+                selected = selected.value == "Notifications",
+                onClick = { selected.value = "Notifications" },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onBackground,
+                    indicatorColor = Color.Transparent
+                )
+            )
+            NavigationBarItem(
+                icon = { Icon(imageVector = Icons.Outlined.Person, contentDescription = null) },
+                selected = selected.value == "Profile",
+                onClick = { selected.value = "Profile" },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = MaterialTheme.colorScheme.primary,
+                    unselectedIconColor = MaterialTheme.colorScheme.onBackground,
+                    indicatorColor = Color.Transparent
+                )
+            )
+        }
+    }
+}
