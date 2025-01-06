dd1cdcb3430adc937de7e0b4dd7597c2187405aa
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2025-01-06 20:28:21 +0300

    AnimatedVisibility added for search bar in HomeScreen

--- app/src/main/java/com/cevdetkilickeser/gardrops/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/cevdetkilickeser/gardrops/ui/screen/home/HomeScreen.kt
@@ -1,5 +1,12 @@
 package com.cevdetkilickeser.gardrops.ui.screen.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -11,19 +18,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.ShoppingBag
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
@@ -31,10 +41,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.cevdetkilickeser.gardrops.R
 import com.cevdetkilickeser.gardrops.ui.screen.home.component.LatestContent
 import com.cevdetkilickeser.gardrops.ui.screen.home.component.PremiumContent
@@ -46,6 +56,11 @@ fun HomeScreen() {
 
     var selectedTabIndex by remember { mutableIntStateOf(0) }
     val tabTitles = listOf("En Yeniler", "Premium")
+    val listState = rememberLazyListState()
+    val isVisible by remember {
+        derivedStateOf { listState.firstVisibleItemIndex == 0 }
+    }
+    val density = LocalDensity.current
 
     Column(
         modifier = Modifier
@@ -69,14 +84,14 @@ fun HomeScreen() {
                     .align(Alignment.CenterEnd)
             ) {
                 IconButton(
-                    onClick = {  }
+                    onClick = { }
                 ) {
                     Icon(
                         imageVector = Icons.Outlined.FavoriteBorder, contentDescription = null
                     )
                 }
                 IconButton(
-                    onClick = {  }
+                    onClick = { }
                 ) {
                     Icon(
                         imageVector = Icons.Outlined.ShoppingBag, contentDescription = null,
@@ -85,46 +100,63 @@ fun HomeScreen() {
                 }
             }
         }
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-
-            placeholder = {
-                Text(
-                    text = "Ara",
-                    color = Color.LightGray,
-                    lineHeight = 8.sp
-                )
-            },
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.Transparent,
-                unfocusedBorderColor = Color.Transparent,
-                focusedContainerColor = SearchBarBackground,
-                unfocusedContainerColor = SearchBarBackground
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            shape = RoundedCornerShape(4.dp),
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        TabRow(
-            selectedTabIndex = selectedTabIndex,
-            modifier = Modifier.fillMaxWidth(),
-            containerColor = Color.Transparent
+        AnimatedVisibility(
+            visible = isVisible,
+            enter = slideInVertically { with(density) { -40.dp.roundToPx() } } + expandVertically(
+                expandFrom = Alignment.Top
+            ) + fadeIn(initialAlpha = 0.3f),
+            exit = slideOutVertically { with(density) { -40.dp.roundToPx() } } + shrinkVertically() + fadeOut()
         ) {
-            tabTitles.forEachIndexed { index, title ->
-                Tab(
-                    selected = selectedTabIndex == index,
-                    onClick = { selectedTabIndex = index },
-                    text = { Text(text = title) },
-                    selectedContentColor = MaterialTheme.colorScheme.primary,
-                    unselectedContentColor = Color.LightGray
+            Column {
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(
+                            text = "Ara",
+                            color = Color.LightGray
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Search,
+                            contentDescription = null
+                        )
+                    },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        focusedContainerColor = SearchBarBackground,
+                        unfocusedContainerColor = SearchBarBackground,
+                        focusedLeadingIconColor = Color.LightGray,
+                        unfocusedLeadingIconColor = Color.LightGray
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    shape = RoundedCornerShape(8.dp),
                 )
+                Spacer(modifier = Modifier.height(16.dp))
+                TabRow(
+                    selectedTabIndex = selectedTabIndex,
+                    modifier = Modifier.fillMaxWidth(),
+                    containerColor = Color.Transparent
+                ) {
+                    tabTitles.forEachIndexed { index, title ->
+                        Tab(
+                            selected = selectedTabIndex == index,
+                            onClick = { selectedTabIndex = index },
+                            text = { Text(text = title) },
+                            selectedContentColor = MaterialTheme.colorScheme.primary,
+                            unselectedContentColor = Color.LightGray
+                        )
+                    }
+                }
             }
         }
+
         when (selectedTabIndex) {
-            0 -> LatestContent {}
+            0 -> LatestContent(listState = listState) {}
             1 -> PremiumContent()
         }
     }
--- app/src/main/java/com/cevdetkilickeser/gardrops/ui/screen/home/component/LatestContent.kt
+++ app/src/main/java/com/cevdetkilickeser/gardrops/ui/screen/home/component/LatestContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,9 +22,11 @@ import com.cevdetkilickeser.gardrops.ui.theme.GardropsTheme
 
 @Composable
 fun LatestContent(
+    listState: LazyListState,
     onClickImageCard: () -> Unit
 ) {
     LazyColumn(
+        state = listState,
         modifier = Modifier
             .padding(horizontal = 8.dp)
             .background(color = Color.Transparent)
@@ -69,6 +72,6 @@ fun LatestContent(
 @Composable
 private fun LatestContentPreview() {
     GardropsTheme {
-        LatestContent(onClickImageCard = {})
+        LatestContent(listState = LazyListState(), onClickImageCard = {})
     }
 }
